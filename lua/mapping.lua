42a50d8150f01bdf24e7059dca4fff8589a12ab6
Julian Merida <julianmr97@gmail.com>
2024-07-15 22:56:19 -0300

    mappings: move lines alt with M-j/k

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -8,6 +8,9 @@ return {
         v = {
           ["<M-up>"] = { ":m '<-2<CR>gv=gv", desc = "Move selection one line up" },
           ["<M-down>"] = { ":m '>+1<CR>gv=gv", desc = "Move selection one line down" },
+
+          ["<M-k>"] = { ":m '<-2<CR>gv=gv", desc = "Move selection one line up" },
+          ["<M-j>"] = { ":m '>+1<CR>gv=gv", desc = "Move selection one line down" },
         },
 
         n = {
@@ -21,13 +24,14 @@ return {
           ["<M-up>"] = { "ddkP", desc = "Move line one line up" },
           ["<M-down>"] = { "ddp", desc = "Move line one line down" },
 
+          ["<M-k>"] = { "ddkP", desc = "Move line one line up" },
+          ["<M-j>"] = { "ddp", desc = "Move line one line down" },
+
           ["<M-z>"] = { ":set wrap!<CR>", desc = "toggle wrap" },
 
           -- ["<C-d>"] = { "<C-d>zz", desc = "Scroll half page down and put cursor in middle of screen" },
           -- ["<C-u>"] = { "<C-u>zz", desc = "Scroll half page up and put cursor in middle of screen" },
 
-          -- tables with the `name` key will be registered with which-key if it's installed
-          -- this is useful for naming menus
           -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
         },
         t = {
