a03854d4b9713d6faf3878831b6c2d0c97010a05
Julian Merida <julianmr97@gmail.com>
2024-07-22 23:22:44 -0300

    Add nvim-treesitter-context to plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
@@ -10,6 +10,13 @@ return {
       "lua",
       "vim",
       -- add more arguments for adding more treesitter parsers
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
+    opts = {
+      mode = "topline",
     },
   },
 }
