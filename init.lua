c6e9e7837e02b142595d387347ca2ce2010d60d3
Julian Merida <julianmr97@gmail.com>
2025-07-14 11:11:11 -0300

    Add command to change colors in vscode-neovim using neovim ui modifier
    See: https://marketplace.visualstudio.com/items?itemName=JulianIaquinandi.nvim-ui-modifier

--- init.lua
+++ init.lua
@@ -17,3 +17,37 @@ end
 
 require "lazy_setup"
 require "polish"
+
+-- Set up theme changer colors for vscode-neovim using neovim ui modifier extension
+-- See: https://marketplace.visualstudio.com/items?itemName=JulianIaquinandi.nvim-ui-modifier
+
+if vim.g.vscode then
+    vim.api.nvim_exec(
+        [[
+    " THEME CHANGER
+    function! SetCursorLineNrColorInsert(mode)
+        " Insert mode: blue
+        if a:mode == "i"
+            call VSCodeNotify('nvim-theme.insert')
+
+        " Replace mode: red
+        elseif a:mode == "r"
+            call VSCodeNotify('nvim-theme.replace')
+        endif
+    endfunction
+
+    augroup CursorLineNrColorSwap
+        autocmd!
+        autocmd ModeChanged *:[vV\x16]* call VSCodeNotify('nvim-theme.visual')
+        autocmd ModeChanged *:[R]* call VSCodeNotify('nvim-theme.replace')
+        autocmd InsertEnter * call SetCursorLineNrColorInsert(v:insertmode)
+        autocmd InsertLeave * call VSCodeNotify('nvim-theme.normal')
+        autocmd CursorHold * call VSCodeNotify('nvim-theme.normal')
+        autocmd ModeChanged [vV\x16]*:* call VSCodeNotify('nvim-theme.normal')
+    augroup END
+]],
+        false
+    )
+
+    return
+end
